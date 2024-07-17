ab6a248ad03f5a87103da35db8cb2ab78b0a0af8
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-17 20:07:47 +0700

    conditional

--- main.go
+++ main.go
@@ -3,9 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	var name string
-	name = "Helmi"
-	name2 := "Adi"
-	var name3 = "Prasetyo"
-	fmt.Println(nama, name2, name3)
+	// // string
+	// var name string
+	// name = "Helmi"
+	// name2 := "Adi"
+	// var name3 = "Prasetyo"
+	// fmt.Println(name, name2, name3)
+
+	// // integer
+	// var x, y int
+	// x = 5
+	// y = 7
+	// result := x * y
+
+	// // float64
+	// var v, w float64
+	// v = 2
+	// w = 4
+	// resulttwo := v / w
+	// fmt.Println(result)
+	// fmt.Println(resulttwo)
+
+	// conditional
+	var angka1, angka2 int64
+	angka1 = 99
+	angka2 = 2
+
+	resultIf := angka1 % angka2
+
+	var isGenap bool
+	if resultIf == 0 {
+		isGenap = true
+	} else {
+		isGenap = false
+	}
+
+	if isGenap {
+		fmt.Println("Dia Bilangan Genap")
+	} else {
+		fmt.Println("Dia Bilangan Gajil")
+	}
 }
